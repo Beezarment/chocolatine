9c37976cfc60068b95a3d2df16cd5e3f8fbbd499
Armand Dufresne <armand.dufresne@epitech.eu>
2024-04-10 16:20:57 +0200

    review

--- src/main.c
+++ src/main.c
@@ -8,10 +8,9 @@
 #include <stdio.h>
 #include "swapupcase.h"
 
-int main(const int ac, char**av) {
+int main(const int ac, char **av)
+{
     if (ac != 2 || av == NULL || av[1] == NULL)
         return EPITECH_KO;
     return swapupcase(av[1]);
-
-    int grosse;
 }
